a803f82550751d214d38049078d9d6ef2949ec3f
Kai Prout <kprout@agiletrailblazers.com>
2015-03-31 16:00:17 -0400

    Adding comments to list controller.

--- Project/app/controllers/list_controller.rb
+++ Project/app/controllers/list_controller.rb
@@ -1,18 +1,17 @@
 class ListController < ApplicationController
   respond_to :json
   def index
-
   end
 
   def save_list
     print params["listName"]
-    list = params
-    ToDo.create(name: list["listName"])
-    render :nothing => true
+    list = params # For readability
+    ToDo.create(name: list["listName"]) # Creates a ToDo object with that name attribute set to the input you gave it.
+    render :nothing => true # Tells rails not to expect the page to change.
   end
 
   def view_lists
-    @lists = ToDo.all
-    render 'list/viewlists'
+    @lists = ToDo.all # Retrieves an array of all ToDo objects in the database.
+    render 'list/viewlists' #Renders the html page in views/list/viewlist.html.erb
   end
 end
